d02437c150059881ec6b4df3d779b585b4663468
migrodcab <miguel.rc95@gmail.com>
2016-04-27 12:00:58 +0200

    Attribute Service

--- Workspace/Acme-Barter/src/main/java/services/AttributeService.java
+++ Workspace/Acme-Barter/src/main/java/services/AttributeService.java
@@ -1,8 +1,15 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Attribute;
+import domain.AttributeDescription;
 import repositories.AttributeRepository;
 
 @Service
@@ -16,6 +23,11 @@ public class AttributeService {
 
 	// Supporting services ----------------------------------------------------
 	
+	@Autowired
+	private ActorService actorService;
+	
+	@Autowired
+	private AttributeDescriptionService attributeDescriptionService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -25,8 +37,56 @@ public class AttributeService {
 
 	// Simple CRUD methods ----------------------------------------------------
 	
+	public Attribute create() {
+		Assert.isTrue(actorService.checkAuthority("ADMIN"), "Only an admin can create an Attribute.");
+		
+		Attribute result;
+		Collection<AttributeDescription> attributesDescription;
+		
+		attributesDescription = new ArrayList<AttributeDescription>();
+		
+		result = new Attribute();
+		
+		result.setAttributesDescription(attributesDescription);
+		
+		return result;
+	}
 	
-
+	public void save(Attribute attribute) {
+		Assert.notNull(attribute);
+		
+		attributeRepository.save(attribute);
+	}
+	
+	/*public void delete(Attribute attribute) {
+		Assert.notNull(attribute);
+		Assert.isTrue(attribute.getId() != 0);
+		Assert.isTrue(actorService.checkAuthority("ADMIN"), "Only an admin can delete attributes");
+				
+		for(AttributeDescription a : attribute.getAttributesDescription()) {
+			a.getItem().removeAttributeDescription(a);
+			
+			attributeDescriptionService.delete(a);
+		}
+		
+		attributeRepository.delete(attribute);
+	}*/
+	
+	public Attribute findOne(int id) {
+		Attribute result;
+		
+		result = attributeRepository.findOne(id);
+		
+		return result;
+	}
+	
+	public Collection<Attribute> findAll() {
+		Collection<Attribute> result;
+		
+		result = attributeRepository.findAll();
+		
+		return result;
+	}
 
 	// Other business methods -------------------------------------------------
 
