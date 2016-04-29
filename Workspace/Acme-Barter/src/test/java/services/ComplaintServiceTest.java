4217cf8728c66ea96e9d45123c7da3338d7351a7
migrodcab <miguel.rc95@gmail.com>
2016-04-29 11:07:52 +0200

    primera prueba crear complaint

--- Workspace/Acme-Barter/src/test/java/services/ComplaintServiceTest.java
+++ Workspace/Acme-Barter/src/test/java/services/ComplaintServiceTest.java
@@ -18,7 +18,9 @@ import utilities.AbstractTest;
 import utilities.InvalidPostTestException;
 import utilities.InvalidPreTestException;
 import domain.Barter;
+import domain.Complaint;
 import domain.Item;
+import domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -43,75 +45,123 @@ public class ComplaintServiceTest extends AbstractTest {
 	
 	@Autowired
 	private MatchService matchService;
+	
+	@Autowired
+	private  UserService userService;
 		
 	// Tests ---------------------------------------
 	
-	/**
-	 * Acme-Six-Pack - 2.0 - Level C - 4.2.1
-	 * The total number of complaints that have been created.
-	 * 
-	 */
-	@Test
-	public void testNumberComplaintsCreated(){
-		// Declare variable
-		int testResult, codeResult;
-		
-		// Load objects to test
-		
-		testResult = complaintService.findAll().size();
-		
-		// Check basic requirements
-		
-		// Execution of test
-		Assert.notNull(null, "A espera del código");
-		
-		// Check results
-		Assert.isTrue(testResult == codeResult);
-	}
+//	/**
+//	 * Acme-Six-Pack - 2.0 - Level C - 4.2.1
+//	 * The total number of complaints that have been created.
+//	 * 
+//	 */
+//	@Test
+//	public void testNumberComplaintsCreated(){
+//		// Declare variable
+//		int testResult, codeResult;
+//		
+//		// Load objects to test
+//		
+//		testResult = complaintService.findAll().size();
+//		
+//		// Check basic requirements
+//		
+//		// Execution of test
+//		Assert.notNull(null, "A espera del código");
+//		
+//		// Check results
+//		Assert.isTrue(testResult == codeResult);
+//	}
+//	
+//	/**
+//	 * Acme-Six-Pack - 2.0 - Level C - 4.2.2
+//	 *  The average number of complaints per barter.
+//	 * 
+//	 */
+//	@Test
+//	public void testAverageComplaintsBarter(){
+//		// Declare variable
+//		double testResult, codeResult;
+//		
+//		// Load objects to test
+//		
+//		testResult = complaintService.findAll().size() / barterService.findAll().size();
+//		
+//		// Check basic requirements
+//		
+//		// Execution of test
+//		Assert.notNull(null, "A espera del código");
+//		
+//		// Check results
+//		Assert.isTrue(testResult == codeResult);
+//	}
+//	
+//	/**
+//	 * Acme-Six-Pack - 2.0 - Level C - 4.2.3
+//	 *  The average number of complaints per match.
+//	 * 
+//	 */
+//	@Test
+//	public void testAverageComplaintsMatch(){
+//		// Declare variable
+//		double testResult, codeResult;
+//		
+//		// Load objects to test
+//		
+//		testResult = complaintService.findAll().size() / matchService.findAll().size();
+//		
+//		// Check basic requirements
+//		
+//		// Execution of test
+//		Assert.notNull(null, "A espera del código");
+//		
+//		// Check results
+//		Assert.isTrue(testResult == codeResult);
+//	}
 	
 	/**
-	 * Acme-Six-Pack - 2.0 - Level C - 4.2.2
-	 *  The average number of complaints per barter.
-	 * 
+	 * Acme-Barter 2.0 - Level c - FR 3.1
+	 * An actor who is authenticated as a user must be able to
+	 * create one or more complaints regarding a barter.
 	 */
-	@Test
-	public void testAverageComplaintsBarter(){
-		// Declare variable
-		double testResult, codeResult;
-		
-		// Load objects to test
-		
-		testResult = complaintService.findAll().size() / barterService.findAll().size();
-		
-		// Check basic requirements
-		
-		// Execution of test
-		Assert.notNull(null, "A espera del código");
-		
-		// Check results
-		Assert.isTrue(testResult == codeResult);
-	}
-	
 	/**
-	 * Acme-Six-Pack - 2.0 - Level C - 4.2.3
-	 *  The average number of complaints per match.
 	 * 
 	 */
 	@Test
-	public void testAverageComplaintsMatch(){
-		// Declare variable
-		double testResult, codeResult;
+	public void testCreateComplaintOfBarterOk() {
+		Complaint complaint;
+		Collection<Complaint> complaints;
+		Collection<Barter> barters;
+		Barter barter;
+		User user;
+		int preSave, postSave;
 		
-		// Load objects to test
+		authenticate("user1");
+		user = userService.findByPrincipal();
+		barter = null;
 		
-		testResult = complaintService.findAll().size() / matchService.findAll().size();
+		barters = barterService.findAll();
+		for(Barter b : barters) {
+			if(b.getUser().getId() != user.getId() && b.getClosed() == false) {
+				barter = b;
+				break;
+			}
+		}
 		
-		// Check basic requirements
+		complaints = complaintService.findAllByBarterOrMatch(barter.getId());
+		preSave = complaints.size();
 		
-		// Execution of test
-		Assert.notNull(null, "A espera del código");
+		complaint = complaintService.create(barter.getId());
+		complaint.setText("prueba");
+		complaint = complaintService.save(complaint);
 		
-		// Check results
-		Assert.isTrue(testResult == codeResult);
+		complaints = complaintService.findAllByBarterOrMatch(barter.getId());
+		postSave = complaints.size();
+		
+		Assert.isTrue(postSave == (preSave + 1));
+		Assert.isTrue(complaints.contains(complaint));
+		
+		authenticate(null);
 	}
 }
